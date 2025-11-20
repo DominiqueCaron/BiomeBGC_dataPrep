052a52d0dd5f5fa18a2681609b7222d6ac90ecfa
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2025-11-20 10:33:25 -0800

    epc write function

--- R/helper.R
+++ R/helper.R
@@ -20,7 +20,7 @@ epcRead <- function(fileName){
   
   # Add attributes
   attr(epcDF, "title") <- "ECOPHYS"
-  attr(epcDF, "decription") <- functionalType
+  attr(epcDF, "description") <- functionalType
   
   return(epcDF)
 }
@@ -44,5 +44,37 @@ epcParseLine <- function(epcLine){
 }
 
 epcWrite <- function(epc, fileName){
-
+  fileName = "test.epc"
+  epc <- c3grass
+  # Create and enter file
+  sink(fileName)
+  # Header line
+  cat("ECOPHYS", rep("", 6), attr(epc, "description"))
+  # Data lines
+  for (i in 1:nrow(epc)){
+    cat("\n")
+    epcValue <- round(epc[i,3], 5)
+    npad1 <- 14 - nchar(as.character(epcValue))
+    epcUnit <- epc[i,2]
+    epcUnit <- paste0("(",epcUnit,")")
+    npad2 <- 10 - nchar(epcUnit)
+    if (grepl("\\*", epcUnit)){
+      epcUnit <- gsub("\\(\\*", "\\*\\(", epcUnit)
+      npad1 <- npad1 - 1
+      npad2 <- npad2 + 1
+    }
+    epcDescription <- epc[i,1]
+    if(epcUnit == "(flag)"){
+      epcDescription <- strsplit(epcDescription, "; ")
+      npad <- 22 - nchar(epcDescription[[1]][1])
+      epcDescription <- paste0(
+        epcDescription[[1]][1], 
+        paste(rep(" ", npad), collapse = ""),
+        epcDescription[[1]][2])
+    }
+    cat(epcValue, rep("", npad1))
+    cat(epcUnit, rep("", npad2))
+    cat(epcDescription)
+  }
+sink()
 }
